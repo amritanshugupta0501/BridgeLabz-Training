3f51503cd326d64cd3f7d3cbe2a4ef2177a9d2e8
Amritanshu Gupta <amritanshugupta0501@gmail.com>
2025-12-20 14:55:11 +0530

    [amritanshu gupta]Added program to display functionality of various access modifiers in C#

--- core-csharp-practice/gcr-codebase/programming-elements/AccessModifiers.cs
+++ core-csharp-practice/gcr-codebase/programming-elements/AccessModifiers.cs
@@ -2,34 +2,34 @@ using System;
 
 class AccessModifiers
 {
-	public int a = 5;								// Public Access Modifier
+	public int num1 = 5;								// Public Access Modifier
 
-	private int n1 = 10;								// Private Access Modifier
+	private int num2 = 10;								// Private Access Modifier
 
-	protected int n2 = 20;								// Protected Access Modifier
+	protected int num3 = 20;								// Protected Access Modifier
 
-	internal int n3 = 100;								// Internal Access Modifier
+	internal int num4 = 100;								// Internal Access Modifier
 
-	protected internal int n4 = 123;						// Protected Internal Access Modifier
+	protected internal int num5 = 123;						// Protected Internal Access Modifier
 
-	// private protected int n5 = 456;
+	// private protected int num6 = 456;
 
 	static void Main()
 	{
-		PublicModifier bam = new PublicModifier();
-		bam.Hello();
+		PublicModifier obj1 = new PublicModifier();
+		obj1.Hello();
 		
-		AccessModifiers mam = new AccessModifiers();
-		mam.showPrivate();
+		AccessModifiers obj2 = new AccessModifiers();
+		obj2.showPrivate();
 
-		DerivedAccessModifiers dam = new DerivedAccessModifiers();
-		dam.deriveAccess();
+		DerivedAccessModifiers obj3 = new DerivedAccessModifiers();
+		obj3.deriveAccess();
 
 	}
 
 	public void showPrivate()
 	{
-		Console.WriteLine("Private Value is: " + n1);
+		Console.WriteLine("Private Value is: " + num2);
 	}
 	
 	
@@ -40,9 +40,9 @@ class PublicModifier
 {
 	public void Hello()
 	{
-		AccessModifiers am = new AccessModifiers();
-		Console.WriteLine("Public value is: "+am.a);
-		Console.WriteLine("Internal value is: "+am.n3);
+		AccessModifiers obj2 = new AccessModifiers();
+		Console.WriteLine("Public value is: "+obj2.num1);
+		Console.WriteLine("Internal value is: "+obj2.num4);
 		
 	}
 }
@@ -52,10 +52,9 @@ class DerivedAccessModifiers : AccessModifiers
 	public void deriveAccess()
 	{
 		
-		Console.WriteLine("Public Value In Derived Class" + a);
-		Console.WriteLine("Internal Value In Derived Class" + n3);
-		Console.WriteLine("ProtectedValue In Derived Class" + n2);
-		Console.WriteLine("protected internal Value In Derived Class" + n4);
-		Console.WriteLine("private protected Value In Derived Class" + n3);
+		Console.WriteLine("Public Value In Derived Class" + num1);
+		Console.WriteLine("Internal Value In Derived Class" + num4);
+		Console.WriteLine("ProtectedValue In Derived Class" + num3);
+		Console.WriteLine("protected internal Value In Derived Class" + num5);
 	}
 }
