fc7086498a02ccb78572d2454e1e72374e25a470
Amritanshu Gupta <amritanshugupta0501@gmail.com>
2026-01-07 16:19:07 +0530

    [Amritanshu Gupta]Refactor Employee Management System UC-04 Application Program

--- oops-csharp-practice/scenario-based/employee-management-system/EmployeeMenu.cs
+++ oops-csharp-practice/scenario-based/employee-management-system/EmployeeMenu.cs
@@ -12,6 +12,7 @@ namespace BridgeLabzTraining.EmployeeWage
         private IEmployee EmployeeUtility;
         public int CountEmployees;
         // Code Snippet to display a menu for the user
+	// UC-04 Switch case used to create menu for the application
         public void EmployeeChoices()
         {
             IEmployeeDetails[] employees = new IEmployeeDetails[100];
