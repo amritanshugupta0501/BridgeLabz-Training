dc2f6646730f8930bf8c293205138131ac915e36
Amritanshu Gupta <amritanshugupta0501@gmail.com>
2026-01-12 16:40:42 +0530

    [Amritanshu Gupta]Added Rod Cutting Scenario Based Program solution

---
+++ data-structures-csharp-practice/scenario-based/metal-factory-pipe-cutting-system/RodCuttingMain.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzTraining.metal_factory_pipe_cutting_system
+{
+    internal class RodCuttingMain
+    {
+        static void Main()
+        {
+            Console.WriteLine("Starting Rod Cutting Analysis Application...");
+            RodCuttingMenu rodCuttingMenu = new RodCuttingMenu();
+            rodCuttingMenu.SearchingForBestCuts();
+            rodCuttingMenu.CheckImpact();
+            rodCuttingMenu.CalculateRevenue();
+            Console.WriteLine("\nExecution Complete. Press any key to exit.");
+            Console.ReadKey();
+        }
+}
