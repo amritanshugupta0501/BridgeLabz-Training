8018c054a59eec6ba323a14f15da6538236fbfaf
Amritanshu Gupta <amritanshugupta0501@gmail.com>
2026-01-12 16:44:13 +0530

    [Amritanshu Gupta]Added Rod Cutting Scenario Based Program solution

---
+++ oops-csharp-practice/scenario-based/metal-factory-pipe-cutting-system/IRodCuttingStrategy.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzTraining.metal_factory_pipe_cutting_system
+{
+    internal interface IRodCuttingStrategy
+    {
+        int GetMaxProfit(int[] priceList, int totalLength);
+        int GetProfit(int[] priceList, int totalLength);
+    }
+}
